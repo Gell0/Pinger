98c5f14c8cb1064dacd9d9750d8d0d24eb95069e
Schmidt Geko <gekoslair@gmail.com>
2021-03-24 03:02:56 +0300

    I did it

---
+++ Pinger/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Pinger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adress:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtIP.Location = new System.Drawing.Point(60, 6);
+            this.txtIP.Multiline = true;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(159, 33);
+            this.txtIP.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ping!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.textBox1.Location = new System.Drawing.Point(15, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 131);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ping Info:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(312, 209);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pinger 1.5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Pinger/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pinger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtIP.Text))
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send("127.0.0.1", 1000);
+                String Status = reply.Status.ToString();
+                String Adress = reply.Address.ToString();
+                String Trip = reply.RoundtripTime.ToString();
+                String TTL = reply.Options.Ttl.ToString();
+                String DF = reply.Options.DontFragment.ToString();
+                String Buffer = reply.Buffer.Length.ToString();
+                string newLine = Environment.NewLine;
+                textBox1.Text = ("Pinging this machine..." + newLine + "Status: " + Status + newLine + "Pinged to: " + Adress + newLine + "Delay: " + Trip + " ms." + newLine + "TTL: " + TTL + newLine + "Fragmentation: " + DF + newLine + "Buffer Size: " + Buffer + " Bytes" + newLine + "Done . . .");
+            }
+            else
+            {
+                try
+                {
+                    Ping ping1 = new Ping();
+                    PingReply reply1 = ping1.Send(txtIP.Text, 1000);
+                    string Status = reply1.Status.ToString();
+                    String Adress = reply1.Address.ToString();
+                    String Trip = reply1.RoundtripTime.ToString();
+                    String TTL = reply1.Options.Ttl.ToString();
+                    String DF = reply1.Options.DontFragment.ToString();
+                    String Buffer = reply1.Buffer.Length.ToString();
+                    string newLine = Environment.NewLine;
+                    textBox1.Text = ("IPStatus: " + Status + newLine + "Adress: " + Adress + newLine + "RoundTripTime: " + Trip + newLine + "TTL: " + TTL + newLine + "Fragmentation: " + DF + newLine + "Buffer Size: " + Buffer + " Bytes" + newLine + "Done . . .");
+                }
+                catch (PingException)
+                {
+                    textBox1.Text = "Adress is unreachable. . .";
+                }
+               
+
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
